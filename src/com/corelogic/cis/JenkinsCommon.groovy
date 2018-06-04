6b0b458c8edd4460f8438930ac1ea86e7b5ee5ae
Mark Webb <mark.webb@cgi.com>
2018-06-04 15:03:13 -0700

    made the code into a Groovy class

--- src/com/corelogic/cis/JenkinsCommon.groovy
+++ src/com/corelogic/cis/JenkinsCommon.groovy
@@ -1,4 +1,7 @@
-def deployToEnv(orgName, spaceName, appName = '') {
+package com.corelogic.cis
+class JenkinsCommon {
+    
+static def deployToEnv(orgName, spaceName, appName = '') {
 
     spaceName = spaceName.toLowerCase()
 
@@ -13,7 +16,7 @@ def deployToEnv(orgName, spaceName, appName = '') {
     executeCFCommand(orgName, spaceName, pushNewCommand)
 }
 
-def executeCFCommand(orgName, spaceName, command) {
+static def executeCFCommand(orgName, spaceName, command) {
     unstash 'build-artifacts'
     unstash 'cf-configs'
 
@@ -28,10 +31,12 @@ def executeCFCommand(orgName, spaceName, command) {
     }
 }
 
-def notifySlack() {
+static def notifySlack() {
     try {
         slackSend(color: '#FFFF00', message: "BUILD/DEPLOY FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})", teamDomain: 'clgx-apptx', token: 'D4jxXWhwNF15AYH9NjVARJcA', channel: '#cis-app-build')
     } catch (e) {
         echo 'slack message failed'
     }
+}
+    
 }
